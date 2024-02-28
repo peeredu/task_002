08e71238e2653ed6cd39c8bad0ec3d26e6fc1969
Olga Komleva <olyakomleva@mail.ru>
2024-02-29 01:06:18 +0300

    add clang-format in workflow

--- src/pascal_functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef PASCAL_FUNCTIONS_H
-#define PASCAL_FUNCTIONS_H
-
-int binom_coeff(int n, int k);
-void find_base_triangle(int *base, int number);
-void output_base_triangle(int *base, int n);
-
-#endif
