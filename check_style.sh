17b236bb1108b0d2bf63593e19e0859821bb60e9
Olga Komleva <olyakomleva@mail.ru>
2024-02-28 21:19:13 +0300

    try again clang6

--- check_style.sh
+++ check_style.sh
@@ -3,14 +3,14 @@
 # Копирование файла .clang-format в текущую директорию
 cp clang/.clang-format .
 
-# Проверка стиля для файлов с расширением .c
-if [ -n "$(clang-format -output-replacements-xml -n -style=file *.c)" ]; then
+# Проверка стиля для файлов с расширением .c в директории src
+if [ -n "$(clang-format -output-replacements-xml -n -style=file src/*.c)" ]; then
     echo "Code style issues found in .c files. Please run clang-format to fix them."
     exit 1
 fi
 
-# Проверка стиля для файлов с расширением .h
-if [ -n "$(clang-format -output-replacements-xml -n -style=file *.h)" ]; then
+# Проверка стиля для файлов с расширением .h в директории src
+if [ -n "$(clang-format -output-replacements-xml -n -style=file src/*.h)" ]; then
     echo "Code style issues found in .h files. Please run clang-format to fix them."
     exit 1
 fi
